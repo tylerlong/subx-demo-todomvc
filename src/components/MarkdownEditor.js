eec1db6a776306f787329c27cc10c8a940dec924
Tyler Long <tyler4long@gmail.com>
2018-08-16 17:06:38 +0800

    Refactor code

---
+++ src/components/MarkdownEditor.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import MarkdownIt from 'markdown-it'
+
+const mdi = new MarkdownIt()
+
+class MarkdownEditor extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = { markdown: '' }
+  }
+  render () {
+    return (
+      <div>
+        <textarea placeholder='Please enter some markdown...' id='markdown-textarea' value={this.state.markdown} onChange={(e) => this.setState({ markdown: e.target.value })} />
+        <div className='markdown-body' dangerouslySetInnerHTML={{ __html: mdi.render(this.state.markdown) }} />
+      </div>
+    )
+  }
+}
+
+export default MarkdownEditor
