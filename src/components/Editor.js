c6a51fd6503d022009bb1ff8745c804650643dfc
Tyler Long <tyler4long@gmail.com>
2018-09-03 22:20:34 +0800

    Correct way to use SubX

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,27 +1,40 @@
 import React from 'react'
+import { debounceTime, map } from 'rxjs/operators'
 
 class Editor extends React.Component {
   constructor (props) {
     super(props)
-    this.rerender = this.rerender.bind(this)
+    this.textChanged = this.textChanged.bind(this)
+    this.htmlChanged = this.htmlChanged.bind(this)
+    this.article = props.article
+    this.state = {
+      text: this.article.text,
+      html: this.article.html()
+    }
+    this.text$ = this.article.text$.pipe(map(() => this.article.text))
+    this.html$ = this.article.text$.pipe(debounceTime(1000), map(() => this.article.html()))
   }
-  rerender () {
-    this.forceUpdate()
+  textChanged (text) {
+    this.setState({ text })
+  }
+  htmlChanged (html) {
+    this.setState({ html })
   }
   componentDidMount () {
-    this.props.article.$.subscribe(this.rerender)
+    this.text$.subscribe(this.textChanged)
+    this.html$.subscribe(this.htmlChanged)
   }
   componentWillUnmount () {
-    this.props.article.$.unsubscribe(this.rerender)
+    this.text$.unsubscribe(this.textChanged)
+    this.html$.unsubscribe(this.htmlChanged)
   }
   render () {
     console.log('render')
-    const { article } = this.props
     return (
       <div>
         <textarea placeholder='Please enter some markdown...' id='markdown-textarea'
-          value={article.text} onChange={e => { article.text = e.target.value }} />
-        <div className='markdown-body' dangerouslySetInnerHTML={{ __html: article.html }} />
+          value={this.state.text} onChange={e => { this.article.text = e.target.value }} />
+        <div className='markdown-body' dangerouslySetInnerHTML={{ __html: this.state.html }} />
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { debounceTime } from 'rxjs/operators'
 import MarkdownIt from 'markdown-it'
 import SubX from 'subx'
 
@@ -10,20 +9,13 @@ import './index.css'
 
 const mdi = new MarkdownIt()
 
-const Article = SubX({ text: '' })
+const Article = SubX({
+  text: ''
+}).computed({
+  html () {
+    return mdi.render(this.text)
+  }
+})
 const article = new Article()
 
-const render = () => {
-  ReactDOM.render((<div>
-    <Editor article={article} />
-  </div>), document.getElementById('root'))
-}
-
-render()
-
-article.$.pipe(
-  debounceTime(1000)
-).subscribe(mutation => {
-  article.html = mdi.render(mutation.val)
-  render()
-})
+ReactDOM.render(<Editor article={article} />, document.getElementById('root'))
