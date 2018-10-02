27a262d952c68af758ab4b2b9530e952baf59298
Tyler Long <tyler4long@gmail.com>
2018-10-02 08:50:39 +0800

    Reset the project

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import TodoApp from './components/TodoApp'
-import Todos from './models/Todos'
-import Todo from './models/Todo'
+import 'todomvc-common/base.css'
+import 'todomvc-app-css/index.css'
 
-import './index.css'
-
-const todos = new Todos()
-todos.todos.push(new Todo({ text: 'hello' }))
-todos.todos.push(new Todo({ text: 'world' }))
-
-ReactDOM.render(<TodoApp todos={todos} />, document.getElementById('root'))
+ReactDOM.render(<div>Hello world</div>, document.getElementById('container'))
--- src/models/Todos.js
+++
@@ -1,7 +0,0 @@
-import SubX from 'subx'
-
-const Todos = SubX({
-  todos: []
-})
-
-export default Todos
