cedcacb7f59d236f6a92f5a1ecc07a128be628d2
Tyler Long <tyler4long@gmail.com>
2018-10-03 16:25:08 +0800

    Fix a werid reference issue

--- src/index.js
+++ src/index.js
@@ -81,11 +81,6 @@ const router = new Router({
 router.init()
 
 class TodoItem extends Component {
-  constructor (props) {
-    super(props)
-    this.todo = this.props.todo
-    this.handleKeyUp = this.handleKeyUp.bind(this)
-  }
   handleKeyUp (e) {
     if (e.key === 'Enter') {
       store.doneEdit(this.todo)
@@ -94,6 +89,8 @@ class TodoItem extends Component {
     }
   }
   render () {
+    this.todo = this.props.todo
+    console.log('render TodoItem')
     return <li className={classNames('todo', { completed: this.todo.completed, editing: this.todo.editing })}>
       <div className='view'>
         <input className='toggle' type='checkbox' checked={this.todo.completed} onChange={e => { this.todo.completed = e.target.checked }} />
@@ -105,7 +102,7 @@ class TodoItem extends Component {
       </div>
       <input ref='editField' className='edit' type='text' value={this.todo.title}
         onChange={e => { this.todo.title = e.target.value }}
-        onKeyUp={this.handleKeyUp}
+        onKeyUp={this.handleKeyUp.bind(this)}
         onBlur={e => store.doneEdit(this.todo)} />
     </li>
   }
@@ -113,6 +110,7 @@ class TodoItem extends Component {
 
 class Main extends Component {
   render () {
+    console.log('render Main')
     const store = this.props.store
     const todos = store.todos
     if (todos.length === 0) {
@@ -130,6 +128,7 @@ class Main extends Component {
 
 class Footer extends Component {
   render () {
+    console.log('render Footer')
     const store = this.props.store
     const todos = store.todos
     if (todos.length === 0) {
@@ -150,12 +149,6 @@ class Footer extends Component {
 }
 
 class App extends Component {
-  constructor (props) {
-    super(props)
-    this.store = this.props.store
-    this.todos = this.store.todos
-    this.handleEnter = this.handleEnter.bind(this)
-  }
   handleEnter (e) {
     if (e.key !== 'Enter') {
       return
@@ -168,11 +161,14 @@ class App extends Component {
     this.todos.push(Todo.create({ title }))
   }
   render () {
+    this.store = this.props.store
+    this.todos = this.store.todos
+    console.log('render App')
     return <>
       <section className='todoapp'>
         <header className='header'>
           <h1>todos</h1>
-          <input className='new-todo' autoFocus autoComplete='off' placeholder='What needs to be done?' onKeyUp={this.handleEnter} />
+          <input className='new-todo' autoFocus autoComplete='off' placeholder='What needs to be done?' onKeyUp={this.handleEnter.bind(this)} />
         </header>
         <Main store={store} />
         <Footer store={store} />
