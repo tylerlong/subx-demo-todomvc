8ea45058d570dee36443b21e2b8de2ab30494e7d
Tyler Long <tyler4long@gmail.com>
2018-10-09 15:48:27 +0800

    Use separate webpack config for dev and prod

--- webpack.config.prod.babel.js
+++ webpack.config.prod.babel.js
@@ -1,9 +1,8 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin'
-import { HotModuleReplacementPlugin } from 'webpack'
 import { join } from 'path'
 
 const config = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/index.js',
   output: {
     path: join(__dirname, 'docs')
@@ -19,25 +18,29 @@ const config = {
       },
       {
         test: /\.js$/,
+        enforce: 'pre',
+        exclude: /(node_modules|docs|test|\.spec\.js)/,
+        use: [
+          {
+            loader: 'webpack-strip-block',
+            options: {
+              start: 'DEV-START',
+              end: 'DEV-END'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.js$/,
         loader: 'babel-loader'
       }
     ]
   },
   devtool: 'source-map',
-  devServer: {
-    contentBase: './build',
-    port: 6066,
-    overlay: {
-      warnings: true,
-      errors: true
-    },
-    hot: true
-  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html'
-    }),
-    new HotModuleReplacementPlugin()
+    })
   ]
 }
 
