1486f923844288af16a763251dd2cc6e679f3f43
Tyler Long <tyler4long@gmail.com>
2018-10-09 15:33:31 +0800

    Update dependencies

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,12 +1,39 @@
 /* eslint-env jest */
 import React from 'react'
 import TestRenderer from 'react-test-renderer'
+import * as R from 'ramda'
+
 import { App } from '../src/components'
 import store from '../src/store'
 
 describe('default', () => {
-  test('default', () => {
+  test('create todos', () => {
     const renderer = TestRenderer.create(<App store={store} />)
     expect(renderer).toBeDefined()
+    expect(store.todos).toEqual([])
+
+    const input = renderer.root.find(el => el.type === 'input' && el.props.className === 'new-todo')
+    input.props.onKeyUp({ key: 'Enter', target: { value: '111' } })
+    expect(R.map(R.dissoc('id'), store.todos)).toEqual([{ title: '111', completed: false }])
+    input.props.onKeyUp({ key: 'Enter', target: { value: '222' } })
+    expect(R.map(R.dissoc('id'), store.todos)).toEqual([
+      { title: '111', completed: false },
+      { title: '222', completed: false }
+    ])
+    input.props.onKeyUp({ key: 'Enter', target: { value: '333' } })
+    expect(R.map(R.dissoc('id'), store.todos)).toEqual([
+      { title: '111', completed: false },
+      { title: '222', completed: false },
+      { title: '333', completed: false }
+    ])
+    input.props.onKeyUp({ key: 'Enter', target: { value: '444' } })
+    input.props.onKeyUp({ key: 'Enter', target: { value: '555' } })
+    expect(R.map(R.dissoc('id'), store.todos)).toEqual([
+      { title: '111', completed: false },
+      { title: '222', completed: false },
+      { title: '333', completed: false },
+      { title: '444', completed: false },
+      { title: '555', completed: false }
+    ])
   })
 })
