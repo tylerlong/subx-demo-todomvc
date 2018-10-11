98c688995ca9797e50749a44ed78c418b7ba30c4
Tyler Long <tyler4long@gmail.com>
2018-10-11 13:12:26 +0800

    Add react array render test case

---
+++ test/react_array_render.spec.js
@@ -0,0 +1,90 @@
+/* eslint-env jest */
+import React from 'react'
+import TestRenderer from 'react-test-renderer'
+import * as R from 'ramda'
+
+let renders = []
+
+describe('react array render', () => {
+  test('default', async () => {
+    class App extends React.Component {
+      constructor (props) {
+        super(props)
+        this.state = {
+          todos: [
+            { title: '111', completed: false },
+            { title: '222', completed: false },
+            { title: '333', completed: false }
+          ]
+        }
+      }
+      render () {
+        renders.push('App')
+        return <>
+          <ul>{this.state.todos.map(todo => <TodoItem todo={todo} key={todo.title} />)}</ul>
+          <button onClick={e => {
+            this.setState({ todos: R.tail(this.state.todos) })
+          }}>Remove first</button>
+        </>
+      }
+    }
+
+    class TodoItem extends React.Component {
+      render () {
+        renders.push('TodoItem')
+        return this.props.todo.title
+      }
+      shouldComponentUpdate (nextProps, nextState) {
+        return this.props.todo.title !== nextProps.todo.title
+      }
+    }
+
+    const renderer = TestRenderer.create(<App />)
+    const button = renderer.root.find(el => el.type === 'button')
+    expect(renders).toEqual(['App', 'TodoItem', 'TodoItem', 'TodoItem'])
+
+    renders = []
+    button.props.onClick()
+    expect(renders).toEqual(['App']) // it doesn't render two remaining TodoItems again.
+  })
+
+  test('without shouldComponentUpdate', async () => {
+    class App extends React.Component {
+      constructor (props) {
+        super(props)
+        this.state = {
+          todos: [
+            { title: '111', completed: false },
+            { title: '222', completed: false },
+            { title: '333', completed: false }
+          ]
+        }
+      }
+      render () {
+        renders.push('App')
+        return <>
+          <ul>{this.state.todos.map(todo => <TodoItem todo={todo} key={todo.title} />)}</ul>
+          <button onClick={e => {
+            this.setState({ todos: R.tail(this.state.todos) })
+          }}>Remove first</button>
+        </>
+      }
+    }
+
+    class TodoItem extends React.Component {
+      render () {
+        renders.push('TodoItem')
+        return this.props.todo.title
+      }
+    }
+
+    renders = []
+    const renderer = TestRenderer.create(<App />)
+    const button = renderer.root.find(el => el.type === 'button')
+    expect(renders).toEqual(['App', 'TodoItem', 'TodoItem', 'TodoItem'])
+
+    renders = []
+    button.props.onClick()
+    expect(renders).toEqual(['App', 'TodoItem', 'TodoItem']) // it DOES render two remaining TodoItems again.
+  })
+})
